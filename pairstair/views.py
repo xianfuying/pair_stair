98ea0e13f730bed92789abec75e383dd6b4f0767
fu ying <xianfuying@gmail.com>
2011-11-20 16:00:56 +0800

    refactor exception to if/else

--- pairstair/views.py
+++ pairstair/views.py
@@ -22,16 +22,19 @@ def stairs(request):
     return render_to_response("stairs.html", RequestContext(request, context))
 
 
+def pair_exist(programmer, programmer_1):
+    return Pair.objects.filter(first=programmer, second=programmer_1).count()
+
+
 def create_pairs(programmers):
     pairs = []
     for programmer in programmers[1:]:
         for programmer_1 in programmers[0:-1]:
             if programmer != programmer_1:
-                try:
-                    pairs.append(Pair.objects.get(first = programmer, second = programmer_1))
-                except :
-#        Exception handling for normal code flow is a bad practice, think of the problems this might have and alternate ways to implement
+                if not pair_exist(programmer, programmer_1):
                     pairs.append(Pair(first=programmer, second=programmer_1, count = 0))
+                else:
+                    pairs.append(Pair.objects.get(first = programmer, second = programmer_1))
             else:
                 break
     return pairs
@@ -40,10 +43,10 @@ def create_pairs(programmers):
 def add(request, firstMember_id, secondMember_id):
     programmer1 = Programmer.objects.get(id=firstMember_id)
     programmer2 = Programmer.objects.get(id=secondMember_id)
-    try:
+    if pair_exist(programmer1, programmer2):
         pair = Pair.objects.get(first = programmer1, second = programmer2)
         pair.count += 1
         pair.save()
-    except :
+    else:
         Pair(first = programmer1, second = programmer2, count = 1).save()
     return redirect(stairs)
